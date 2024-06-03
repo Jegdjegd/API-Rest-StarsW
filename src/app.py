2e08a08dce68f8edea58852f8edf57c9ee526d29
Jhoann <64670851+Jegdjegd@users.noreply.github.com>
2024-06-03 10:02:24 +0000

    Ajustes relaciones de tablas

--- src/app.py
+++ src/app.py
@@ -6,9 +6,10 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
+from utils import APIException, generate_sitemap #importamos APIExcepcion para poder hacer las validaciones
 from admin import setup_admin
-from models import db, User, Planet, Character, FavoriteCharacter, FavoritePlanet
+from models import db, User, Characters, Planets #importamos la tabla a quien queremos hacer la consulta
+#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -25,287 +26,191 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+# generate sitemap with all your endpoints/# generar un mapa del sitio con todos sus puntos finales(endpoints)
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 
+#COLOCAMOS NUESTROS ENDPOINTS A PARTIR DE
 
-# users below
-@app.route('/usuarios', methods=['GET'])
-def get_users():
-    all_users = list(map(lambda user: user.serialize(), User.query.all()))
-    return jsonify(all_users), 200
+#USER PLURALMENTE
+@app.route('/user', methods=['GET'])
+def handle_userAll():
 
-@app.route('/usuarios', methods=["POST"]) 
-def create_user():
-    data = request.json
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
+    users = User.query.all() #almacenamos todos los usuarios
+    data = [user.serialize() for user in users ] # para cada usuario dentro de los usuarios me los serialice
 
-    if not username or not email or not password: return jsonify({'error': 'missing field'}), 400
+    return jsonify(data), 200 #aqui lo convertimos en string con jsonify
 
-    existing_user = User.query.filter_by(email=email).first()
-    if existing_user: return jsonify({'error': 'email is already in use'}), 400
-
-    new_user = User(username=username, email=email, password=password)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({
-        'id': new_user.id,
-        'username': new_user.username,
-        'email': new_user.email
-    }), 201
-
-@app.route("/usuarios/<int:position>", methods=["GET"])
-def get_user(position):
-    user = User.query.get(position)
+#USER SINGLE
+@app.route('/user/<int:id>', methods=['GET'])
+def get_userSingle(id):
+    user = User.query.get(id) #se obtiene el usuario de la base de datos utilizando User.query.get(id)
+    
+    if user is None: #Si user es None, se lanza la excepción APIException con el mensaje "This user does not exist" y el codigo de estado 400.
+        raise APIException('This user does not exist', status_code=400)
+    
     return jsonify(user.serialize()), 200
 
-@app.route("/usuarios/<int:position>", methods=["DELETE"])
-def delete_user(position):
-    user = User.query.get(position)
+#CREATE NEW USER (POST)
+@app.route('/user', methods = ['POST']) #ruta para aceptar solicitudes y crear nuevo usuario
+def create_user(): #def create_user(): Esta función llamada create_user se ejecutará cuando se acceda a la ruta /user mediante una solicitud POST.
 
-    if not user: return jsonify({'error': 'not found'}), 404
+#se guarda el contenido en la variable "body"
+    body = request.json #request.json contiene los datos enviados con la solicitud POST.
 
-    db.session.delete(user)
-    db.session.commit()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'name' not in body or not body ['name']:  #se verifica que esta agregada la propiedad name y que no se ingrese el campo vacio
+        raise APIException("You need to specify the name, can't be empty" , status_code=400)
+    if 'email' not in body or not body['email']:
+        raise APIException("You need to specify the email, can't be empty", status_code=400)
 
-    return jsonify({'message': 'deleted successfully'}), 200
+    user = User(email=body["email"], name=body["name"]) #se crea un objeto de la clase User con los valores del correo electrónico y el estado activo obtenidos del objeto body.
+    db.session.add(user) #esto indica que se debe crear un nuevo registro en la base de datos con los valores proporcionados.
+    db.session.commit() #Realiza una confirmación en la sesión de la base de datos, lo que efectivamente guarda los cambios realizados en la base de datos.
 
-@app.route("/usuarios/<int:position>", methods=["PUT"])
-def update_user(position):
-    user = User.query.get(position)
+    response_body = {
+        "msg": "El usuario ha sido creado",
+    } # Creamos un diccionario llamado response_body que contiene un mensaje indicando que el usuario ha sido creado.
 
-    if not user: return jsonify({'error': 'not found'}), 404
+    return jsonify(response_body), 200 #La función jsonify se utiliza para convertir el diccionario en una respuesta JSON valida
 
-    data = request.json
-    new_username = data.get('username')
-    new_email = data.get('email')
-    new_password = data.get('password')
+#DELETE USER (ID)
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_userSingle(id): #esta funcion recibe el parámetro id, que se obtiene de la URL.
+        #Se verifica si user existe. Si no existe, se devuelve una respuesta con un mensaje de "User not found" y un codigo de estado 404.
+        #Si el usuario existe, se elimina de la base de datos usando db.session.delete(user). Luego, se confirma la transacción con db.session.commit().
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'User not found'}), 404
 
-    if new_username: user.username = new_username
-    if new_email: user.email = new_email
-    if new_password: user.password = new_password
-
-    db.session.commit()
-
-    return jsonify(user.serialize()), 200
-
-
-
-
-
+# ALL THE CHARACTERS
 @app.route('/characters', methods=['GET'])
-def get_characters():
-    all_characters = list(map(lambda character: character.serialize(), Character.query.all()))
-    return jsonify(all_characters), 200
+def handle_charactersAll():
 
-@app.route('/characters', methods=["POST"]) 
-def create_character():
-    data = request.json
-    name = data.get('name')
-    homeworld_id = data.get('homeworld')
+    charactersAll = Characters.query.all() #almacenamos todos los usuarios
+    data = [character.serialize() for character in charactersAll] # para cada character dentro de los characters y nos lo serialice
+    return jsonify(data), 200 #aqui lo convertimos en string con jsonify
 
-    if not name or not homeworld_id: return jsonify({'error': 'missing field'}), 400
+#CHARACTERS SINGLE
+@app.route('/characters/<int:id>', methods = ['GET'])
+def get_characterSingle(id):
+    characters = Characters.query.get(id)
+
+    if characters is None: #Si characters es None(no existe el id ingresado), se lanza la excepción APIException con el mensaje "This user does not exist" y el codigo de estado 400.
+        raise APIException('This Character does not exist', status_code=400)
     
-    homeworld = Planet.query.get(homeworld_id)
+    print(characters.serialize())
+    return jsonify(characters.serialize()), 200
 
-    if not homeworld: return jsonify({'error': 'invalid homeworld id'}), 400
+#CREATE NEW CHARACTER
+@app.route('/characters', methods = ['POST']) #ruta para aceptar solicitudes y crear nuevo usuario
+def create_newCharacter(): #def create_newCharacte(): Esta función se ejecutará cuando se acceda a la ruta /user mediante una solicitud POST.
+ #se guarda el contenido en la variable "body"
+    body = request.json
 
-    existing_character = Character.query.filter_by(name=name).first()
-    if existing_character: return jsonify({'error': 'character is already on the database'}), 400
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'name' not in body or not body['name']:
+        raise APIException("You need to specify the name, can't be empty", status_code=400)
+    if 'gender' not in body or not body['gender']:
+        raise APIException("You need to specify the gender,can't be empty" , status_code=400)
+    if 'eye_color' not in body or not body['eye_color']:
+        raise APIException("You need to specify the eye_color, can't be empty" , status_code=400)
 
-    new_character = Character(name=name, homeworld_id=homeworld_id)
+    print(body)
+    characters = Characters(name = body["name"], gender=body["gender"], eye_color=body["eye_color"]) #se crea un objeto de la clase User con los valores del correo electrónico y el estado activo obtenidos del objeto body.
+    db.session.add(characters) #esto indica que se debe crear un nuevo registro en la base de datos con los valores proporcionados.
+    db.session.commit() #Realiza una confirmación en la sesión de la base de datos, lo que efectivamente guarda los cambios realizados en la base de datos.
 
-    db.session.add(new_character)
+    response_body = {
+         "msg": "a new character has been added",
+     } # Creamos un diccionario llamado response_body que contiene un mensaje indicando que el usuario ha sido creado.
+
+    return jsonify(response_body), 200 #La función jsonify se utiliza para convertir el diccionario en una respuesta JSON valida
+
+#DELETE CHARACTER(ID)
+@app.route('/characters/<int:id>', methods=['DELETE'])
+def delete_charactersSingle(id): #esta funcion recibe el parámetro id, que se obtiene de la URL.
+
+    characters = Characters.query.get(id)
+    if characters:
+        db.session.delete(characters)
+        db.session.commit()
+        return jsonify({'message': 'character deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Character not found'}), 404
+
+# ALL THE PLANETS
+@app.route('/planets', methods=["GET"])
+def handle_planetsAll():
+    planetsAll= Planets.query.all()
+    data = [planet.serialize() for planet in planetsAll] # para cada planet dentro de los planetaS y nos lo serialice
+
+    return jsonify(data), 200
+
+#PLANET SINGLE(ID)
+@app.route('/planets/<int:id>', methods=["GET"])
+def get_planetSingle(id):
+
+    planet = Planets.query.get(id)
+
+    if planet is None: #Si el planet es None(no existe el id ingresado), se lanza la excepción APIException con el mensaje "This user does not exist" y el codigo de estado 400.
+        raise APIException('This Planet does not exist', status_code=400)
+    
+    print(planet.serialize())
+    return (jsonify.serialize()), 200
+
+#CREATE NEW PLANET
+@app.route('/planets', methods=["POST"])
+def create_newPlanet():
+
+    body = request.json
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'name' not in body or not body['name'].strip():
+        raise APIException("You need to specify the name, can't be empty", status_code=400)
+    if 'terrain' not in body or not body['terrain']:
+        raise APIException("You need to specify the terrain can't be empty", status_code=400)
+    if 'population' not in body or not body['population']:
+        raise APIException("You need to specify the population, can't be empty", status_code=400)
+    
+    print(body)
+    planets = Planets(name=body["name"], terrain=body["terrain"], population=body["population"])
+    db.session.add(planets) #esto indica que se debe crear un nuevo registro en la base de datos con los valores proporcionados.
     db.session.commit()
 
-    return jsonify({
-        'id': new_character.id,
-        'name': new_character.name,
-        'homeworld_id': new_character.homeworld_id,
-        'homeworld': homeworld.name,
-    }), 201
+    response_body = {
+         "msg": "a new planet has been added",
+     } # Creamos un diccionario llamado response_body que contiene un mensaje indicando que el nuevo planeta ha sido creado.
 
-@app.route("/characters/<int:position>", methods=["GET"])
-def get_character(position):
-    character = Character.query.get(position)
-    if character: return jsonify(character.serialize()), 200
-    else: return jsonify({'error': 'not found'}), 404
+    return jsonify(response_body), 200
 
-@app.route("/characters/<int:position>", methods=["DELETE"])
-def delete_character(position):
-    character = Character.query.get(position)
+#DELETE PLANET (ID)
+@app.route('/planets/<int:id>', methods = ["DELETE"])
+def delete_planet(id):
+    planet = Planets.query.get(id)
+    if planet:
+        db.session.delete(planet) #Esta línea elimina el objeto planet de la sesión de la base de datos.
+        db.session.commit() # se guarda los cambios realizados en la base de datos. La eliminación del planeta se confirma al llamar a este método.
+        return jsonify({'message': 'planet deleted succesfully'})
+    else:
+        return jsonify({'message':'Planet not found'}), 400
 
-    if not character: return jsonify({'error': 'not found'}), 404
 
-    db.session.delete(character)
-    db.session.commit()
-
-    return jsonify({'message': 'deleted successfully'}), 200
-
-@app.route("/characters/<int:position>", methods=["PUT"])
-def update_character(position):
-    character = Character.query.get(position)
-
-    if not character: return jsonify({'error': 'not found'}), 404
-
-    data = request.json
-    new_name = data.get('name')
-    new_homeworld_id = data.get('homeworld')
-
-    existing_character = Character.query.filter_by(name=new_name).first()
-    if existing_character: return jsonify({'error': 'character with that name is already on the database'}), 400
-
-    homeworld = Planet.query.get(new_homeworld_id)
-    if new_homeworld_id and not homeworld: return jsonify({'error': 'invalid homeworld id'}), 400
-
-    if new_name: character.name = new_name
-    if new_homeworld_id: character.homeworld_id = new_homeworld_id
-
-    db.session.commit()
-
-    return jsonify(character.serialize()), 200
-
-@app.route('/planets', methods=['GET'])
-def get_planets():
-    all_planets = list(map(lambda planet: planet.serialize(), Planet.query.all()))
-    return jsonify(all_planets), 200
-
-@app.route('/planets', methods=["POST"]) 
-def create_planet():
-    data = request.json
-    name = data.get('name')
-
-    if not name: return jsonify({'error': 'missing field'}), 400
-
-    existing_planet = Planet.query.filter_by(name=name).first()
-    if existing_planet: return jsonify({'error': 'planet already exists in the database'}), 400
-
-    new_planet = Planet(name=name)
-
-    db.session.add(new_planet)
-    db.session.commit()
-
-    return jsonify({
-        'id': new_planet.id,
-        'name': new_planet.name,
-    }), 201
-
-@app.route("/planets/<int:position>", methods=["GET"])
-def get_planet(position):
-    planet = Planet.query.get(position)
-    return jsonify(planet.serialize()), 200
-
-@app.route("/planets/<int:position>", methods=["DELETE"])
-def delete_planet(position):
-    planet = Planet.query.get(position)
-
-    if not planet: return jsonify({'error': 'not found'}), 404
-
-    db.session.delete(planet)
-    db.session.commit()
-
-    return jsonify({'message': 'deleted successfully'}), 200
-
-@app.route("/planets/<int:position>", methods=["PUT"])
-def update_planet(position):
-    planet = Planet.query.get(position)
-
-    if not planet: return jsonify({'error': 'not found'}), 404
-
-    data = request.json
-    new_name = data.get('name')
-
-    if new_name: planet.name = new_name
-
-    db.session.commit()
-
-    return jsonify(planet.serialize()), 200
-
-@app.route('/users/<int:position>/favorites', methods=["GET"])
-def get_fav(position):
-    favorite_planets = FavoritePlanet.query.filter_by(user_id=position).all()
-    favorite_characters = FavoriteCharacter.query.filter_by(user_id=position).all()
-    
-    favorites = favorite_planets + favorite_characters
-    
-    serialized_favorites = [favorite.serialize() for favorite in favorites]
-    
-    return jsonify(serialized_favorites), 200
-
-@app.route('/users/<int:position>/favorites/characters', methods=["GET"])
-def get_fav_char(position):
-    favorite_characters = FavoriteCharacter.query.filter_by(user_id=position).all()
-    serialized_favorites = [favorite.serialize() for favorite in favorite_characters]
-    
-    return jsonify(serialized_favorites), 200
-
-@app.route('/users/<int:position>/favorites/characters', methods=["POST"])
-def add_fav_char(position):
-    data = request.json
-    character_id = data.get('character_id')
-    existing_character = Character.query.filter_by(id=character_id).first()
-    if not existing_character: return jsonify({'error': 'character with provided id does not exist'}), 400
-
-    new_fav = FavoriteCharacter(character_id=character_id, user_id=position)
-    
-    db.session.add(new_fav)
-    db.session.commit()
-    
-    return jsonify(new_fav.serialize()), 201
-
-@app.route('/users/<int:position>/favorites/characters/<int:index>', methods=["DELETE"])
-def delete_fav_char(index):
-    fav = FavoriteCharacter.query.get(index)
-
-    if not fav: return jsonify({'error': 'not found'}), 404
-
-    db.session.delete(fav)
-    db.session.commit()
-
-    return jsonify({'message': 'deleted successfully'}), 200
-
-@app.route('/users/<int:position>/favorites/planets', methods=["GET"])
-def get_fav_planet(position):
-    favorite_planets = FavoritePlanet.query.filter_by(user_id=position).all()
-    serialized_favorites = [favorite.serialize() for favorite in favorite_planets]
-    
-    return jsonify(serialized_favorites), 200
-
-@app.route('/users/<int:position>/favorites/planets', methods=["POST"])
-def add_fav_planet(position):
-    data = request.json
-    planet_id = data.get('planet_id')
-    existing_planet = Planet.query.filter_by(id=planet_id).first()
-    if not existing_planet: return jsonify({'error': 'planet with provided id does not exist'}), 400
-
-    new_fav = FavoritePlanet(planet_id=planet_id, user_id=position)
-    
-    db.session.add(new_fav)
-    db.session.commit()
-    
-    return jsonify(new_fav.serialize()), 201
-
-@app.route('/users/<int:position>/favorites/planets/<int:index>', methods=["DELETE"])
-def delete_fav_planet(index):
-    fav = FavoritePlanet.query.get(index)
-
-    if not fav: return jsonify({'error': 'not found'}), 404
-
-    db.session.delete(fav)
-    db.session.commit()
-
-    return jsonify({'message': 'deleted successfully'}), 200
-
+# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,75 +2,102 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Planet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Planet %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-        }
-    
-class Character(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    homeworld_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
-    homeworld = db.relationship('Planet')
-
-    def __repr__(self):
-        return '<Character %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "homeworld": self.homeworld.serialize() if self.homeworld else None,
-        }
-
 class User(db.Model):
+    __tablename__ = 'user'  #__tablename__ Especifica el nombre de la tabla en la base de datos para cada clase.
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    favorites_planets = db.relationship('FavoritesPlanets', backref='user', lazy=True) #Se definen relaciones entre las tablas utilizando la función relationship de SQLAlchemy.
+    favorites_characters = db.relationship('FavoritesCharacters', backref='user', lazy=True) #Se definen relaciones entre las tablas utilizando la función relationship de SQLAlchemy.
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return self.email   #__repr__() se utiliza para proporcionar una representación de cadena legible de un objeto.
 
-    def serialize(self):
+    def serialize(self):  #En este caso, el método serialize() devuelve un diccionario que contiene tres claves: "id", "name" y "email".
+                          # El valor de "id" se obtiene del atributo self.id, el valor de "name" se obtiene del atributo self.name,
         return {
             "id": self.id,
-            "name": self.username,
             "email": self.email,
+            "name": self.name,
+            # do not serialize the password, its a security breach
         }
-    
-class FavoritePlanet(db.Model):
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    # Aquí definimos columnas para la dirección de la tabla.
+    # Tenga en cuenta que cada columna también es un atributo de instancia de Python normal.
     id = db.Column(db.Integer, primary_key=True)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
-    planet = db.relationship('Planet')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User')
+    name = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+            #backref='characters' crea un atributo en la tabla characters que permite acceder a los registros relacionados en la tabla FavoritesCharacters, y 
+        # lazy=True configura la carga diferida de los registros relacionados, cargandolos solo cuando se accede a ellos explicitamente.
+    favorites_character = db.relationship('FavoritesCharacters', backref='characters', lazy=True) #backref en SQLAlchemy es utilizada para establecer una relacion
+                                                                                            #bidireccional entre las tablas. En este caso, cuando se establece 
+                                                                                            # backref='characters' en la relación de la tabla FavoritesCharacters, se crea 
+                                                                                            # automáticamente un atributo adicional en la tabla Characters que permite 
+                                                                                            # acceder a los registros relacionados en la tabla FavoritesCharacters
+    def __repr__(self):
+        return '<Characters %r>' %  self.name #
 
     def serialize(self):
         return {
             "id": self.id,
-            "planet_id": self.planet_id,
-            "planet_name": self.planet.name,
+            "name": self.name,
         }
     
-class FavoriteCharacter(db.Model):
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    # Aquí definimos columnas para la dirección de la tabla.
+    # Tenga en cuenta que cada columna también es un atributo de instancia de Python normal.
     id = db.Column(db.Integer, primary_key=True)
-    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
-    character = db.relationship('Character')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User')
+    name = db.Column(db.String(250), nullable=False) 
+    terrain = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250), nullable=False)
+    favorites_planets = db.relationship('FavoritesPlanets', backref='planets', lazy=True) #lazy=True se utiliza para configurar la carga diferida (lazy loading) de los 
+                                                                                        #registros relacionados. Con lazy=True, los registros relacionados se cargarán 
+                                                                                        # solo cuando se acceda a ellos explícitamente. Esto puede ser util para mejorar el
+                                                                                        #  rendimiento, ya que evita la carga de todos los registros relacionados de forma 
+                                                                                        # automatica.
+    def __repr__(self):
+        return self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "character_id": self.character_id,
-            "character_name": self.character.name,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population":self.population
         }
+
+
+class FavoritesCharacters(db.Model):
+    __tablename__ = "favoritescharacters"
+    id = db.Column (db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=False)
+
+    def __repr__(self):
+        return str(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.user_id,
+        }    
+
+class FavoritesPlanets(db.Model):
+    __tablename__ = "favoritesplanets"
+    id = db.Column (db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return str(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.user_id
+        }    
+
